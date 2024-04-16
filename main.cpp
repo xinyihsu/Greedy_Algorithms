47a15fb589971ab5a65639a5e286ff02d001bafd
xinyihsu <114647677+xinyihsu@users.noreply.github.com>
2024-04-17 00:06:12 +0800

    init

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main()
+{
+    int lecturesNum = 20;
+    vector<pair<float, float>> lecturesTime = {{9, 10},{11, 16},{12.5, 13.5},
+        {10.5, 16},{9.5, 14},{10.5, 13},{11.5, 15.5},{12.5, 16.5},{11, 12},
+        {9.5, 14},{11.5, 17},{9.5, 15.5},{12, 17.5},{11.5, 14},{12.5, 15},
+        {11, 16},{12, 17},{15.5, 16.5},{16.5, 17.5},{9, 16}};
+    for (int i = 0; i < lecturesNum; i++) {
+        if (lecturesTime[])
+    }
+}
